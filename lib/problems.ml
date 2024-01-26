5b852793b5bf276f7c0aaee2ec8d2f317d1ccb37
JNSFilipe <jose.filipe@ieee.org>
2024-01-26 18:06:42 +0000

    Solved Problem 2

--- lib/problems.ml
+++ lib/problems.ml
@@ -1,2 +1,8 @@
 (* <++> Problem 1 *)
 let rec last = function [] -> None | [ x ] -> Some x | _ :: t -> last t
+
+(* <++> Problem 2 *)
+let rec last_two = function
+  | [] | [ _ ] -> None
+  | [ x; y ] -> Some (x, y)
+  | _ :: t -> last_two t
--- test/unittests.ml
+++ test/unittests.ml
@@ -7,10 +7,24 @@ let () =
       (* <++> Problem 1 *)
       ( "Problem 1 - last",
         [
-          test_case "last" `Quick (fun () ->
-              check (option string) "last" (Some "d")
+          test_case "Input [ a; b; c; d ]" `Quick (fun () ->
+              check (option string) "last 1" (Some "d")
                 (last [ "a"; "b"; "c"; "d" ]));
-          test_case "last empty" `Quick (fun () ->
-              check (option string) "last empty" None (last []));
+          test_case "Input []" `Quick (fun () ->
+              check (option string) "last 2" None (last []));
+        ] );
+      (* <++> Problem 2 *)
+      ( "Problem 2 - last_two",
+        [
+          test_case "Input [ a; b; c; d ]" `Quick (fun () ->
+              check
+                (option (pair string string))
+                "last_two 1"
+                (Some ("c", "d"))
+                (last_two [ "a"; "b"; "c"; "d" ]));
+          test_case "Input [a]" `Quick (fun () ->
+              check
+                (option (pair string string))
+                "last_two 2" None (last_two [ "a" ]));
         ] );
     ]
